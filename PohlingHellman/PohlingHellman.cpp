ada37208d9ac55838734249fbd35147035509331
fmauri <mauri95francesco@gmail.com>
2018-05-12 12:29:45 +0200

    upate pohling hellman xi search

--- PohlingHellman/PohlingHellman.cpp
+++ PohlingHellman/PohlingHellman.cpp
@@ -5,14 +5,43 @@
 #include "PohlingHellman.h"
 #include "../PollardRho/PollarRho.h"
 
+/*
+ * TODO the g has to have change (check note) , before going to next iteration
+ * TODO for each factor => inner loop for i = 0 => exponent ;
+ */
 NTL::ZZ PohlingHellman::searchResult() {
     NTL::ZZ result = NTL::ZZ(0);
-    NTL::ZZ g, h, x;
+    NTL::ZZ g, h, x, divisor;
+    std::vector<NTL::ZZ> allXi;
+    long y = 0;
     for (const auto &factor:factors) {
         g = alpha ^ (N / factor.result);
         h = beta ^ (N / factor.result);
-        PollarRho pollarRho(g, h, factor.result);
+        /*
+         * Search for x
+         */
+        g = NTL::PowerMod(g, NTL::power(factor.prime, factor.exponent - 1), N);
+        h = NTL::PowerMod(h, NTL::power(factor.prime, factor.exponent - 1), N);
+        PollarRho pollarRho(g, h, N, factor.prime);
         x = pollarRho.searchXParallelPollard();
+        allXi.push_back(x);
+        for (long i = factor.exponent - 2; i > 0; i++) {
+            divisor = 0;
+            y = 0;
+            for (auto const &exponent : allXi) {
+                divisor = divisor + (exponent * NTL::power(factor.prime, y));
+                y++;
+            }
+            g = g / divisor;
+            h = h / divisor;
+            pollarRho.setNewValues(g, h);
+            x = pollarRho.searchXParallelPollard();
+            allXi.push_back(x);
+        }
+        x = 0;
+        for (auto const &item:allXi) {
+            x += item;
+        }
         x_factors.push_back(x);
     }
     result = calcCRT();
