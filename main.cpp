c0ab597cecadc483d9f49814d07249a9d957ae1a
fmauri <mauri95francesco@gmail.com>
2018-05-14 22:18:27 +0200

    update pohling

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ int main() {
     /*
      * PollarRho
      */
-    std::cout << "Pollard Rho\n";
-    PollarRho pollarRho;
-    pollarRho.printLog();
-    result = pollarRho.searchXParallelPollard();
-    std::cout << "x = " << result << std::endl;
-    /*
-     * Kangaroo Weiner
-     */
-    std::cout << "\nKangaroo Weiner\n";
-    WeinerKangaroo weinerKangaroo;
-    weinerKangaroo.printLog();
-    result = weinerKangaroo.searchCollisions();
-    std::cout << "x = " << result << std::endl;
+//    std::cout << "Pollard Rho\n";
+//    PollarRho pollarRho;
+//    pollarRho.printLog();
+//    result = pollarRho.searchXParallelPollard();
+//    std::cout << "x = " << result << std::endl;
+//    /*
+//     * Kangaroo Weiner
+//     */
+//    std::cout << "\nKangaroo Weiner\n";
+//    WeinerKangaroo weinerKangaroo;
+//    weinerKangaroo.printLog();
+//    result = weinerKangaroo.searchCollisions();
+//    std::cout << "x = " << result << std::endl;
     /*
      * PohlingHellman
      */
